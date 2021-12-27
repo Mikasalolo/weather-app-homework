0b94719b008901aeba4fc3a9beab48890e80a204
Mikasalolo <nobuesh@gmail.com>
2021-12-27 18:56:02 -0500

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+}
+
+let apiKey = "e7ad0124ee7d5ca0900acdf1494227cb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
