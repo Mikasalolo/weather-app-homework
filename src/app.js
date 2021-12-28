547282417d7f36f71e94a7fb998206a0ef8ad0af
Mikasalolo <nobuesh@gmail.com>
2021-12-27 22:11:17 -0500

    updated city search

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "e7ad0124ee7d5ca0900acdf1494227cb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
